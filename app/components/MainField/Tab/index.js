97fcc903238ac0578a923f71bd7d4ea5d2e89d03
pordee <pdumrhi@sertiscorp.com>
2017-01-06 09:59:26 +0700

    assign array tab

--- app/components/MainField/Tab/index.js
+++ app/components/MainField/Tab/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 class Tab extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    generateTab() {
+        const ss = ['s', 'ss'];
+        const tab = ss.map(item=><div>{item}</div>);
+        // const tab = this.props.tabs.map(item=><div>{item}</div>);
+        return tab;
+    }
+
     render() {
         return(
     <div>
-    Tab
+        {this.generateTab()}
     </div>);}
 }
+
+Tab.propTypes = {
+    tabs: PropTypes.array
+};
 
 const mapStateToProps = (state) => {
     return {
