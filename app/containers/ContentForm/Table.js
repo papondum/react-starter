01ba2bb1771b47c36c95f3dfad99a6b7249f87ea
Kitnipan Dermtoranin <kitnipan@dezainwerkz.com>
2017-04-17 00:57:05 +0700

    🚑 Fixed multiple selection

--- app/containers/ContentForm/Table.js
+++ app/containers/ContentForm/Table.js
@@ -12,23 +12,23 @@ const MenuItemGroup = Menu.ItemGroup;
 
 const OverlayMenu = ({ onClick, columns, unselected }) => (
   <Menu onClick={onClick}>
-      <Menu.Item key="asc"><Icon type="arrow-up" /> Sort Ascending</Menu.Item>
-      <Menu.Item key="desc"><Icon type="arrow-down" /> Sort Descending</Menu.Item>
-      <SubMenu key="filter" title={[<Icon type="filter" />, <span>Filter using</span>]}>
-          <MenuItemGroup>
-              <Menu.Item key="equal">Equal</Menu.Item>
-              <Menu.Item key="not equal">Not equal</Menu.Item>
-              <Menu.Item key="less than">Less than</Menu.Item>
-              <Menu.Item key="greater than">Greater than</Menu.Item>
-              <Menu.Item key="less than or">Less than or equal to</Menu.Item>
-              <Menu.Item key="greater than or">Greater than or equal to</Menu.Item>
-          </MenuItemGroup>
-      </SubMenu>
-      <SubMenu key="columns" title={[<Icon type="layout" />, <span>Column</span>]}>
-          <MenuItemGroup>
-              {columns.map(column => {
-                  return <Menu.Item key={column}><input type="checkbox" checked={!unselected.includes(column)} /> {column}</Menu.Item>
-              })}
+    <Menu.Item key="asc"><Icon type="arrow-up" /> Sort Ascending</Menu.Item>
+    <Menu.Item key="desc"><Icon type="arrow-down" /> Sort Descending</Menu.Item>
+    <SubMenu key="filter" title={[<Icon type="filter" />, <span>Filter using</span>]}>
+      <MenuItemGroup>
+        <Menu.Item key="equal">Equal</Menu.Item>
+        <Menu.Item key="not equal">Not equal</Menu.Item>
+        <Menu.Item key="less than">Less than</Menu.Item>
+        <Menu.Item key="greater than">Greater than</Menu.Item>
+        <Menu.Item key="less than or">Less than or equal to</Menu.Item>
+        <Menu.Item key="greater than or">Greater than or equal to</Menu.Item>
+      </MenuItemGroup>
+    </SubMenu>
+    <SubMenu key="columns" title={[<Icon type="layout" />, <span>Column</span>]}>
+      <MenuItemGroup>
+        {columns.map(column => {
+          return <Menu.Item key={column}><input type="checkbox" checked={!unselected.includes(column)} /> {column}</Menu.Item>
+        })}
       </MenuItemGroup>
     </SubMenu>
   </Menu>
@@ -56,9 +56,6 @@ class CustomTable extends React.Component {
     this.setState({
       columns: nextProps.columns,
     })
-  }
-  getSelected() {
-    return this.state.selected;
   }
   setFilterState(key, value) {
     const filters = Object.assign({}, this.state.filters);
@@ -155,11 +152,6 @@ class CustomTable extends React.Component {
       return b[key].localeCompare(a[key]);
     }
   }
-  select(id) {
-    this.setState({
-      selected: this.state.selected.concat(id).reduce(getUnique, []),
-    })
-  }
   renderColumn(column) {
     if (column.includes('Date')) {
       return <DatePicker
@@ -170,12 +162,12 @@ class CustomTable extends React.Component {
     }
     if (['Document Status', 'Salesperson', 'Status'].includes(column)) {
       return <Select
-          style={{ width: 300 }}
-          value={this.state.filters[column]}
-          onChange={v => this.setFilterState([column], v || '')}
-          allowClear
-             >
-          {this.props.content.reduce((p, c) => p.includes(c[column]) ? p : p.concat(c[column]), []).map(c => <Option key = {c} value={c}>{c}</Option>)}
+        style={{ width: 300 }}
+        value={this.state.filters[column]}
+        onChange={v => this.setFilterState([column], v || '')}
+        allowClear
+        >
+        {this.props.content.reduce((p, c) => p.includes(c[column]) ? p : p.concat(c[column]), []).map(c => <Option key = {c} value={c}>{c}</Option>)}
       </Select>
     }
     return <input
@@ -191,81 +183,86 @@ class CustomTable extends React.Component {
     const filterColumns = this.state.bodyFilters.columns
     const columns = this.props.columns.filter(column => !filterColumns.includes(column))
     return columns.map((column, index) => ({
-      title: (<div key = {index}>
+      title: (
+        <div key = {index}>
           <div>
-              {this.renderColumn(column)}
+            {this.renderColumn(column)}
           </div>
           <div style={{ padding: '5px 5px',
-          border: '0.5px #ccc solid' }}>
-              <Dropdown placement="bottomLeft" overlay={<OverlayMenu
-                  onClick={e => this.sortingMenu(column, e)}
-                  columns={this.state.columns}
-                  unselected={this.state.bodyFilters.columns} />}>
-              <a className="ant-dropdown-link" href="#">{column}</a>
+            border: '0.5px #ccc solid' }}>
+            <Dropdown
+              placement="bottomLeft"
+              overlay={<OverlayMenu
+                onClick={e => this.sortingMenu(column, e)}
+                columns={this.state.columns}
+                unselected={this.state.bodyFilters.columns} />}
+                ><a className="ant-dropdown-link" href="#">{column}</a>
             </Dropdown>
           </div>
-        </div>),
-        dataIndex: column,
-        key: column,
-        width: `${1 / columns.length * 100}%`,
-        onCellClick: record => this.props.rowClicked(record.id),
-        render: (t, record) => index === 0 ? (
-          <div key={t}>
-              <input
-                  onClick={() => {
-                      this.setState({ editItem: record.id })
-                      this.props.checkedSingleItem(record.id)
-                  }}
-                  type='checkbox'
-                  checked={this.state.editItem === record.id}
-              /> {t}
-          </div>
-          ) : <div key={t}>{t}</div>
-        }))
-        .concat({
-          title: <div><div><Icon onClick={this.clearFilter.bind(this)} style={{ cursor: 'pointer' }} type="filter" /></div><br /></div>,
-          render: () => ''
-        })
-      }
-      render () {
-        return (
-          <LocaleProvider locale={enUS}>
-              <div>
-                  <Table
-                      pagination={false}
-                      scroll={{ x: 120, y: this.props.subContent.length > 0 ? '40vh' : '73vh' }}
-                      columns={this.columns()}
-                      dataSource={this.filterBody}
-                  />
-                  {this.props.subContent.length > 0 &&
-                      <div className="subcontent-table">
-                          <div className='action-bar' style={{ paddingBottom: 10 }}>
-                              <h2>{this.props.header}</h2>
-                          </div>
-                          <Table
-                              pagination={false}
-                              scroll={{ x: 120, y: '20vh' }}
-                              columns={Object.keys(this.props.subContent[0]).map(key => ({
-                                  title: <div style={{ padding: '5px 5px',
-                                      border: '0.5px #ccc solid',
-                                  }}>{key}</div>,
-                                  key,
-                                  dataIndex: key,
-                                  width: `${1/Object.keys(this.props.subContent[0]).length * 100}%`
-                              }))}
-                              dataSource={this.props.subContent}
-                          />
-                      </div>
-                  }
+        </div>
+      ),
+      dataIndex: column,
+      key: column,
+      width: `${1 / columns.length * 100}%`,
+      onCellClick: record => this.props.rowClicked(record.id),
+      render: (t, record) => index === 0 ? (
+        <div key={t}>
+          <input
+            onClick={() => {
+              this.props.toggleItem(record.id)
+              this.props.checkedSingleItem(record.id)
+            }}
+            type='checkbox'
+            checked={this.props.selected.includes(record.id)}
+            />
+          <span style={{ marginLeft: 10 }}>{t}</span>
+        </div>
+      ) : <div key={t}>{t}</div>
+    }))
+    .concat({
+      title: <div><div><Icon onClick={this.clearFilter.bind(this)} style={{ cursor: 'pointer' }} type="filter" /></div><br /></div>,
+      render: () => ''
+    })
+  }
+  render () {
+    return (
+      <LocaleProvider locale={enUS}>
+        <div>
+          <Table
+            pagination={false}
+            scroll={{ x: 120, y: this.props.subContent.length > 0 ? '40vh' : '73vh' }}
+            columns={this.columns()}
+            dataSource={this.filterBody}
+            />
+          {this.props.subContent.length > 0 &&
+            <div className="subcontent-table">
+              <div className='action-bar' style={{ paddingBottom: 10 }}>
+                <h2>{this.props.header}</h2>
+              </div>
+              <Table
+                pagination={false}
+                scroll={{ x: 120, y: '20vh' }}
+                columns={Object.keys(this.props.subContent[0]).map(key => ({
+                  title: <div style={{ padding: '5px 5px',
+                    border: '0.5px #ccc solid',
+                  }}>{key}</div>,
+                  key,
+                  dataIndex: key,
+                  width: `${1/Object.keys(this.props.subContent[0]).length * 100}%`
+                }))}
+                dataSource={this.props.subContent}
+                />
             </div>
-          </LocaleProvider>
-        )
-      }
-    }
+          }
+        </div>
+      </LocaleProvider>
+    )
+  }
+}
 
-    const getUnique = (p, c) => {
-      if (p.includes(c)) return p;
-      return p.concat(c);
-    }
+const getUnique = (p, c) => {
+  if (p.includes(c)) return p;
+  return p.concat(c);
+}
 
-    export default CustomTable;
+export default CustomTable;
--- app/containers/ContentForm/index.js
+++ app/containers/ContentForm/index.js
@@ -11,7 +11,7 @@ class ContentForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      checkedItem: [],
+      selected: [],
       subContent: [],
       subContent: [],
     }
@@ -19,64 +19,68 @@ class ContentForm extends React.Component {
 
   componentWillReceiveProps(nextProps){
     if(this.props.type !== nextProps.type){
-      this.setState({ checkedItem:[], subContent: [] })
+      this.setState({ selected:[], subContent: [] })
     }
     if(nextProps.deleteCall.userAcc === 'active'){
-      let obj = { user_id: this.state.checkedItem }
+      let obj = { user_id: this.state.selected }
       this.props.deleteItem(obj, 'User account')
     }
     if(nextProps.deleteCall.userRole === 'active'){
-      let obj = { role_id: this.state.checkedItem }
+      let obj = { role_id: this.state.selected }
       this.props.deleteItem(obj, 'User role')
     }
     if(nextProps.deleteCall.customer === 'active'){
-      let obj = { customer_id:this.state.checkedItem }
+      let obj = { customer_id:this.state.selected }
       this.props.deleteItem(obj, 'Customer')
     }
     if(nextProps.deleteCall.supplier === 'active'){
-      let obj = { supplier: this.state.checkedItem }
+      let obj = { supplier: this.state.selected }
       this.props.deleteItem(obj, 'Supplier')
     }
     if(nextProps.deleteCall.price === 'active'){
-      let obj = { pricelist_id:  this.state.checkedItem }
+      let obj = { pricelist_id:  this.state.selected }
       this.props.deleteItem(obj, 'Price list')
     }
     if(nextProps.deleteCall.product === 'active'){
-      let obj = { product_id:  this.state.checkedItem }
+      let obj = { product_id:  this.state.selected }
       this.props.deleteItem(obj, 'Product')
     }
     if(nextProps.deleteCall.brand === 'active'){
-      let obj = { brand_id: this.state.checkedItem }
+      let obj = { brand_id: this.state.selected }
       this.props.deleteItem(obj, 'Brand')
     }
     if(nextProps.deleteCall.film === 'active'){
-      let obj = { film_id: this.state.checkedItem }
+      let obj = { film_id: this.state.selected }
       this.props.deleteItem(obj, 'Film Type')
     }
     if(nextProps.deleteCall.grade === 'active'){
-      let obj = { grade_id: this.state.checkedItem }
+      let obj = { grade_id: this.state.selected }
       this.props.deleteItem(obj, 'Grade')
     }
     if(nextProps.deleteCall.quotation === 'active'){
-      let obj = { quotation_id: this.state.checkedItem }
+      let obj = { quotation_id: this.state.selected }
       this.props.deleteItem(obj, 'Quotation')
     }
     if(nextProps.deleteCall.sales === 'active'){
-      let obj = { order_id: this.state.checkedItem }
+      let obj = { order_id: this.state.selected }
       this.props.deleteItem(obj, 'Sales Order')
     }
     if(nextProps.deleteCall.purchase === 'active'){
-      let obj = { purchase_id: this.state.checkedItem }
+      let obj = { purchase_id: this.state.selected }
       this.props.deleteItem(obj, 'Purchase Order')
     }
   }
-  get subContentHeader() {
-    switch (this.props.type) {
-      case 'Quotation': return 'Quotation Line(s)'
-      case 'Sales Order': return 'Sales Order Line(s)'
-      case 'Purchase Order': return 'Purchase Order Line(s)'
-      default: return ''
+  toggleItem(item) {
+    if (this.state.selected.includes(item)) {
+      return this.setState({
+        editItem: null,
+        selected: this.state.selected.filter(s => s !== item),
+      })
     }
+    this.setState({
+      editItem: item,
+      selected: this.state.selected.concat(item).reduce(getUnique, []),
+    })
   }
   rowClicked(id) {
     switch (this.props.type) {
@@ -96,19 +100,28 @@ class ContentForm extends React.Component {
     }
   }
 
+  get subContentHeader() {
+    switch (this.props.type) {
+      case 'Quotation': return 'Quotation Line(s)'
+      case 'Sales Order': return 'Sales Order Line(s)'
+      case 'Purchase Order': return 'Purchase Order Line(s)'
+      default: return ''
+    }
+  }
   get columns() {
     if (this.props.content.length === 0) {
       return []
     }
     return Object.keys(this.props.content[0]).filter(key => key !== 'id')
   }
-
   render() {
     if (this.props.content.length === 0) {
       return <div></div>
     }
     return (<div>
       <Table
+        toggleItem={item => this.toggleItem(item)}
+        selected={this.state.selected}
         header={this.subContentHeader}
         checkedSingleItem={this.props.checkedSingleItem}
         rowClicked={id => this.rowClicked(id)}
@@ -129,5 +142,9 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(Object.assign({},DeleteActions), dispatch)
 }
 
+const getUnique = (p, c) => {
+  if (p.includes(c)) return p;
+  return p.concat(c);
+}
 
 export default connect(mapStateToProps,mapDispatchToProps)(ContentForm)
