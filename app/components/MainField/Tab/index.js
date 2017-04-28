dbd3341cd7456005bcca530858a03dae49080bb4
Pathompond Yamdach <pathompond.y@appman.co.th>
2017-04-28 14:01:59 +1000

    tab-close with img-x

--- app/components/MainField/Tab/index.js
+++ app/components/MainField/Tab/index.js
@@ -2,6 +2,8 @@ import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import { closeTab } from '../../../actions';
 import './style.scss';
+import cancelIcon from '../../../resource/Icon/button_cancel.png';
+
 class Tab extends React.Component {
     constructor(props) {
         super(props);
@@ -11,7 +13,7 @@ class Tab extends React.Component {
     generateTab() {
       console.log('test');
         const tab = this.props.tab.tabs;
-        const tabs = tab.map(item=><div key={item} onClick={()=>this.props.openContent(item)} className="tab-style">{item}<span lassName="tab-close" onClick={()=>this.props.closeTab(item)}>x</span></div>);
+        const tabs = tab.map(item=><div key={item} onClick={()=>this.props.openContent(item)} className="tab-style">{item}<span lassName="tab-close" onClick={()=>this.props.closeTab(item)}><img src={cancelIcon}/></span></div>);
         return tabs;
     }
 
--- app/components/MainField/TabList/index.js
+++ app/components/MainField/TabList/index.js
@@ -1,5 +1,7 @@
 import React, {PropTypes} from 'react';
 import './style.scss';
+import cancelIcon from '../../../resource/Icon/button_cancel.png';
+
 class TabList extends React.Component {
     constructor(props) {
         super(props);
@@ -10,7 +12,7 @@ class TabList extends React.Component {
         const tab = this.props.tab.tabs;
         const tabs = tab.map(item=>{
           return (<div key={item}  className={this.props.tab.activeTabs==item? 'active-tab tab-style':"tab-style"}>
-            <span onClick={()=>this.props.openContent(item)}>{item}</span> <span className="tab-close" onClick={()=>this.props.closeTab(item)}>x</span>
+            <span onClick={()=>this.props.openContent(item)}>{item}</span> <span className="tab-close" onClick={()=>this.props.closeTab(item)}><img src={cancelIcon}/></span>
           </div>)});
         return tabs;
     }
