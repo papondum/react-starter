3ea358cd530ff6a370f21aac100956b537337694
pordee <pdumrhi@sertiscorp.com>
2017-04-29 22:27:55 +0700

    fix tab overflow

--- app/components/MainField/TabList/index.js
+++ app/components/MainField/TabList/index.js
@@ -9,7 +9,7 @@ class TabList extends React.Component {
     generateTab() {
         const tab = this.props.tab.tabs;
         const tabs = tab.map(item=>{
-          return (<div key={item}  className={this.props.tab.activeTabs==item? 'active-tab tab-style':"tab-style"}>
+          return (<div key={item}  className={this.props.tab.activeTabs==item? 'active-tab tab-style flex':"tab-style flex"}>
             <span onClick={()=>this.props.openContent(item)}>{item}</span> <span onClick={()=>this.props.closeTab(item)}>x</span>
           </div>)});
         return tabs;
